0f767e23bfed1bf503f2d9530c68f8e7bdedfe66
Pavan Kumar Sahu <pavankumarsahu243@gmail.com>
2025-04-04 12:52:55 +0530

    Update main.py

--- BillingApp/main.py
+++ BillingApp/main.py
@@ -35,4 +35,5 @@ def index():
     return render_template('index.html', menu=MENU)
 
 if __name__ == '__main__':
-    app.run(debug=True)
+    app.run(app.run(host='0.0.0.0', port=5000, debug=True)
+ 
